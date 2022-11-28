f9a266a78e8a038f564f7c947e8b9ecd41f62e5e
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-11-29 02:06:29 +0600

    load buyers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.Port || 5000;
 
@@ -13,6 +14,23 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1luet9i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next){
+    console.log('token gg', req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(401).send({message: 'unsuthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run(){
    try{
@@ -51,9 +69,15 @@ async function run(){
         res.send(category);
     });
 
-    app.get('/orders', async(req, res) =>{
+    app.get('/orders',  async(req, res) =>{
         const email = req.query.email;
-        console.log(email);
+        console.log(req.headers.authorization);
+        // const decodedEmail = req.decoded.email;
+
+        // if(email !== decodedEmail){
+        //     return res.status(403).send({message: 'forbidden access'});
+        // }
+
         const query = { email: email };
         const cursor = ordersCollection.find(query);
         const order = await cursor.toArray();
@@ -65,6 +89,23 @@ async function run(){
         const result = await ordersCollection.insertOne(booking);
         res.send(result);
     })
+
+    app.get('/jwt', async(req, res) =>{
+        const email = req.query.email;
+        const query ={email: email};
+        const user = await buyersCollection.findOne(query);
+        if(user){
+            const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+            return res.send({accessToken: token});
+        }
+        res.status(403).send({accessToken: ''})
+    })
+
+    app.get('/buyers', async(req, res) =>{
+        const query = {};
+        const result = await buyersCollection.find(query).toArray();
+        res.send(result);
+    } )
 
     app.post('/buyers', async(req, res) =>{
         const user = req.body;
